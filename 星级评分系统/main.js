51d647b1082daa39e05e3a4425dcd5227bb7acdb
ylm <yingleiming@outlook.com>
2019-10-15 23:06:23 +0800

    添加onmouseover事件

--- 星级评分系统/main.js
+++ 星级评分系统/main.js
@@ -12,14 +12,19 @@ window.onload=function(){
         lis[i].onmouseover=function () {
             console.log(this.index);
             if(this.index===0){
+                document.getElementById("point").innerText="1分";
                 document.getElementById("script").innerText=scriptMsg[0].msg;
             }else if(this.index===1){
+                document.getElementById("point").innerText="2分";
                 document.getElementById("script").innerText=scriptMsg[1].msg;
             }else if(this.index===2){
+                document.getElementById("point").innerText="3分";
                 document.getElementById("script").innerText=scriptMsg[2].msg;
             }else if(this.index===3){
+                document.getElementById("point").innerText="4分";
                 document.getElementById("script").innerText=scriptMsg[3].msg;
             }else if(this.index===4){
+                document.getElementById("point").innerText="5分";
                 document.getElementById("script").innerText=scriptMsg[4].msg;
             }
         }
